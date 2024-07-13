e37259e9f917158c96ddf3e1fd918b85b895bf6a
juanjose <juanjose1343@icloud.com>
2024-07-13 15:40:07 -0600

    1-add pytest

--- tests/api/test_query.py
+++ tests/api/test_query.py
@@ -27,3 +27,23 @@ def test_entities_thing():
     response = client.post("/", json={"query": query})
     assert response.status_code == 200
     assert response.json() == {"data": {"_entities": [{"id": "1", "name": "Thing"}]}}
+
+# Test case for fetching advisors
+def test_fetch_advisors():
+    query = '''
+    query {
+        allAdvisors {
+            id
+            name
+            expertise
+        }
+    }
+    '''
+    
+    response = client.post("/", json={"query": query})
+
+    assert response.status_code == 200
+    json_data = response.json()
+
+    assert json_data["data"]["allAdvisors"][0]["name"] == "Advisor One"
+    assert json_data["data"]["allAdvisors"][1]["name"] == "Advisor Two"
